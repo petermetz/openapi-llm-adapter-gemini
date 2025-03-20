ad6dbe103f51019d71ad11dbb38256622cefcf98
Peter Somogyvari <peter.metz@unarin.com>
2025-03-19 19:41:04 -0700

    chore: publish v1.0.1 - fixing type exports in package.json
    Signed-off-by: Peter Somogyvari <peter.metz@unarin.com>

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
-// Mapper
+// Gemini Mapper
 export { IOpenApiContext } from "./gemini/mapper.js";
 export { IOpenApiSpecToGeminiToolsResponse } from "./gemini/mapper.js";
-export { OperationTypeNames } from "./openapi/operation-type-names.js";
-export { isV3RequestBodyObject } from "./openapi/is-v3-request-body-object.js";
-export { isV3_1RequestBodyObject } from "./openapi/is-v31-request-body-object.js";
+export { E_REF_NOT_SUP } from "./gemini/mapper.js";
+export { W_OA_PAR_NON } from "./gemini/mapper.js";
 export { mapSpecsToTools } from "./gemini/mapper.js";
 
 // Runner
-export { Runner } from "./gemini/runner.js";
+export { Runner, newRunner } from "./gemini/runner.js";
 
 // OpenAPI
+export { OperationTypeNames } from "./openapi/operation-type-names.js";
+export { isV3_1RequestBodyObject } from "./openapi/is-v31-request-body-object.js";
+export { isV3RequestBodyObject } from "./openapi/is-v3-request-body-object.js";
 export { isV3ReferenceObject } from "./openapi/is-v3-reference-object.js";
